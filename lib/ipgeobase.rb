e57ebe006edf009890a99ed3a19069cd9695cac7
HEX <thehexreader@gmail.com>
2022-03-17 11:46:15 +0300

    cleaning

--- lib/ipgeobase.rb
+++ lib/ipgeobase.rb
@@ -1,29 +1,29 @@
 # frozen_string_literal: true
 
-require_relative "ipgeobase/version"
+require_relative 'ipgeobase/version'
 
 require 'addressable/uri'
 require 'happymapper'
 require 'net/http'
 
+
+# This module gets metadata by ip
 module Ipgeobase
-    class Error < StandardError; end
+  class Error < StandardError; end
 
-    def self.lookup(ip)
-        uri = Addressable::URI.parse("http://ip-api.com/xml/#{ip}")
-        response = Net::HTTP.get_response(uri)
-        Geo_Base.parse(response.body) if response.is_a?(Net::HTTPSuccess)
-    end
+   def self.lookup(ip)
+    uri = Addressable::URI.parse("http://ip-api.com/xml/#{ip}")
+    response = Net::HTTP.get_response(uri)
+    Geo_Base.parse(response.body) if response.is_a?(Net::HTTPSuccess)
+  end
 
-    class Geo_Base
-        include HappyMapper
-
-        tag 'query'
-        
-        element :city, String, tag: 'city'
-        element :country, String, tag: 'country'
-        element :countryCode, String, tag: 'countryCode'
-        element :lat, Float, tag: 'lat'
-        element :lon, Float, tag: 'lon'
-    end
+  class Geo_Base
+    include HappyMapper
+    tag 'query'
+    element :city, String, tag: 'city'
+    element :country, String, tag: 'country'
+    element :countryCode, String, tag: 'countryCode'
+    element :lat, Float, tag: 'lat'
+    element :lon, Float, tag: 'lon'
+  end
 end
--- test/test_ipgeobase.rb
+++ test/test_ipgeobase.rb
@@ -2,44 +2,47 @@
 
 require_relative '../lib/ipgeobase'
 
-require "test_helper"
+require 'test_helper'
 
 class TestIpgeobase < Minitest::Test
-    def setup
-        @test_values = {
-            city: 'Ashburn',
-            country: 'United States',
-            countryCode: 'US',
-            lat: 39.03,
-            lon: -77.5
-        }
-    end
+
+  @stub_request_body = '
+  <?xml version"1.0" encoding="UTF-8"?>
+  <query>
+    <status>success</status>
+    <country>United States</country>
+    <countryCode>US</countryCode>
+    <region>VA</region>
+    <regionName>Virginia</regionName>
+    <city>Ashburn</city>
+    <zip>20149</zip>
+    <lat>39.03</lat>
+    <lon>-77.5</lon>
+    <timezone>America/New_York</timezone>
+    <isp>Google LLC</isp>
+    <org>Google Public DNS</org>
+    <as>AS15169 Google LLC</as>
+    <query>8.8.8.8</query>
+  </query>'
+
+  def setup
+    @test_values = {
+      city: 'Ashburn',
+      country: 'United States',
+      countryCode: 'US',
+      lat: 39.03,
+      lon: -77.5
+    }
+  end
     
-    def test_lookup
-        stub_request(:get, "http://ip-api.com/xml/8.8.8.8").to_return(status: 200,
-        body: '<?xml version="1.0" encoding="UTF-8"?>
-               <query>
-                 <status>success</status>
-                 <country>United States</country>
-                 <countryCode>US</countryCode>
-                 <region>VA</region>
-                 <regionName>Virginia</regionName>
-                 <city>Ashburn</city>
-                 <zip>20149</zip>
-                 <lat>39.03</lat>
-                 <lon>-77.5</lon>
-                 <timezone>America/New_York</timezone>
-                 <isp>Google LLC</isp>
-                 <org>Google Public DNS</org>
-                 <as>AS15169 Google LLC</as>
-                 <query>8.8.8.8</query>
-               </query>')
-        
-        input = Ipgeobase.lookup('8.8.8.8')
-        assert(input.city == @test_values[:city])
-        assert(input.country == @test_values[:country])
-        assert(input.countryCode == @test_values[:countryCode])
-        assert(input.lat == @test_values[:lat])
-        assert(input.lon == @test_values[:lon])
-    end
+  def test_lookup
+    stub_request(:get, 'http://ip-api.com/xml/8.8.8.8').to_return(status: 200,
+    body: @stub_request_body)
+    input = Ipgeobase.lookup('8.8.8.8')
+    assert(input.city == @test_values[:city])
+    assert(input.country == @test_values[:country])
+    assert(input.countryCode == @test_values[:countryCode])
+    assert(input.lat == @test_values[:lat])
+    assert(input.lon == @test_values[:lon])
+  end
 end
