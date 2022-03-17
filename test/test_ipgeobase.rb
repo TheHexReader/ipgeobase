a76b2583b32b323a10ebd4396205adb485adb677
HEX <thehexreader@gmail.com>
2022-03-17 12:11:13 +0300

    cleaning

--- test/test_ipgeobase.rb
+++ test/test_ipgeobase.rb
@@ -6,32 +6,19 @@ require 'test_helper'
 
 class TestIpgeobase < Minitest::Test
   def setup
-    @test_values = {
-      city: 'Ashburn',
-      country: 'United States',
-      countryCode: 'US',
-      lat: 39.03,
-      lon: -77.5
-    }
+    @test_values = {city: 'Ashburn', country: 'United States',
+      countryCode: 'US', lat: 39.03, lon: -77.5}
     stub_request_body = '<?xml version="1.0" encoding="UTF-8"?>
-    <query>
-    <status>success</status>
-    <country>United States</country>
-    <countryCode>US</countryCode>
-    <region>VA</region>
-    <regionName>Virginia</regionName>
-    <city>Ashburn</city>
-    <zip>20149</zip>
-    <lat>39.03</lat>
-    <lon>-77.5</lon>
-    <timezone>America/New_York</timezone>
-    <isp>Google LLC</isp>
-    <org>Google Public DNS</org>
-    <as>AS15169 Google LLC</as>
-    <query>8.8.8.8</query>
-    </query>'
+    <query><status>success</status><country>United States</country>
+    <countryCode>US</countryCode><region>VA</region>
+    <regionName>Virginia</regionName><city>Ashburn</city>
+    <zip>20149</zip><lat>39.03</lat><lon>-77.5</lon>
+    <timezone>America/New_York</timezone><isp>Google LLC</isp>
+    <org>Google Public DNS</org><as>AS15169 Google LLC</as>
+    <query>8.8.8.8</query></query>'
     stub_request(:get, 'http://ip-api.com/xml/8.8.8.8').to_return(status: 200, body: stub_request_body)
   end
+
   def test_lookup
     input = Ipgeobase.lookup('8.8.8.8')
     assert(input.city == @test_values[:city])
