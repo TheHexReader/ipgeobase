75bd12eea1dff85e3cc04117ccf2a2e0f4e2739a
HEX <thehexreader@gmail.com>
2022-03-21 12:21:21 +0300

    correcting mistakes

--- lib/ipgeobase.rb
+++ lib/ipgeobase.rb
@@ -1,28 +1,18 @@
 # frozen_string_literal: true
 
 require_relative 'ipgeobase/version'
+require_relative 'ipgeobase/geobase'
 
 require 'addressable/uri'
-require 'happymapper'
 require 'net/http'
+
+IP_API_URL = 'http://ip-api.com/xml/'
 
 # This module gets metadata by ip
 module Ipgeobase
-  class Error < StandardError; end
-
   def self.lookup(ip)
-    uri = Addressable::URI.parse("http://ip-api.com/xml/#{ip}")
+    uri = Addressable::URI.parse(IP_API_URL + ip)
     response = Net::HTTP.get_response(uri)
     GeoBase.parse(response.body) if response.is_a?(Net::HTTPSuccess)
-  end
-
-  class GeoBase
-    include HappyMapper
-    tag 'query'
-    element :city, String, tag: 'city'
-    element :country, String, tag: 'country'
-    element :countryCode, String, tag: 'countryCode'
-    element :lat, Float, tag: 'lat'
-    element :lon, Float, tag: 'lon'
   end
 end
---
+++ lib/ipgeobase/geobase.rb
@@ -0,0 +1,11 @@
+require 'happymapper'
+
+class GeoBase
+    include HappyMapper
+    tag :query
+    element :city, String, tag: 'city'
+    element :country, String, tag: 'country'
+    element :countryCode, String, tag: 'countryCode'
+    element :lat, Float, tag: 'lat'
+    element :lon, Float, tag: 'lon'
+end
